14973a01d5bcca19183da2326fe5ae573b44ceca
pandoragear <andy.dang.it@gmail.com>
2021-03-04 22:18:22 -0600

    Add log for first name

--- Page2/app.js
+++ Page2/app.js
@@ -1,14 +1,18 @@
 function fetchData() {
-  fetch("http://data.nba.net/10s/prod/v1/2020/players.json/jkl;jasdf")
+  fetch("http://data.nba.net/10s/prod/v1/2020/players.json")
     .then((response) => {
-        console.log(response);
-        if (!response.ok) {
-            throw Error("Error");
-        }
+      if (!response.ok) {
+        throw Error("Error");
+      }
       return response.json();
     })
     .then((data) => {
       console.log(data.league.standard);
+      const html = data.league.standard.map(user =>{
+          return `<p> Name: ${user.firstName} </p>` 
+      })
+      console.log(html);
+      document.querySelector("#stat").insertAdjacentHTML("afterbegin", "<h1>test </h1>"); 
     })
     .catch((error) => {
       console.log(error);

5780921295503f2fa1cd1799c3d50e288c9c5026
pandoragear <andy.dang.it@gmail.com>
2021-03-04 22:05:58 -0600

    Error loging for Fetch

--- Page2/app.js
+++ Page2/app.js
@@ -1,11 +1,18 @@
-function fetchData(){
-   fetch("http://data.nba.net/10s/prod/v1/2020/players.json").then(response => {
-     return response.json()
-
-   }).then(data => {
-       console.log(data.league.standard);
-   })
+function fetchData() {
+  fetch("http://data.nba.net/10s/prod/v1/2020/players.json/jkl;jasdf")
+    .then((response) => {
+        console.log(response);
+        if (!response.ok) {
+            throw Error("Error");
+        }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.league.standard);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 fetchData();
-

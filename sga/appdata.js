2b58e7ca9c8f10d08ea45cf114cf332267e487e5
pandoragear <andy.dang.it@gmail.com>
2021-03-05 23:43:36 -0600

    Add comments

--- sga/appdata.js
+++ sga/appdata.js
@@ -3,6 +3,7 @@
 function fetchData() {
   fetch("http://data.nba.net/data/10s/prod/v1/2020/players/1628983_profile.json")
     .then((response) => {
+      // will throw and error if cannot load page 
       if (!response.ok) {
         throw Error("Error");
       }
@@ -10,7 +11,11 @@ function fetchData() {
     })
     .then((data) => {
       console.log(data.league.standard.stats.latest);
+
+      // profile = where the data is point to inside the api 
       const profile = data.league.standard.stats.latest;
+
+      // getting that specific data and assign it
       const ppg = profile.ppg;
       const rpg = profile.rpg;
       const apg = profile.apg;
@@ -21,6 +26,8 @@ function fetchData() {
       const turnovers = profile.turnovers;
       const point = profile.assists;
       const gamesPlayed = profile.gamesPlayed;
+
+      // place the api data onto hmtl 
 
       document.querySelector("#ppg").innerHTML = `ppg: ${ppg}` ; 
       document.querySelector("#rpg").innerHTML = `rpg: ${rpg}` ; 
